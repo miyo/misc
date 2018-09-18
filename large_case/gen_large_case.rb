e3bb1aee70a9fdb0f722b318a9862e8ce9f4cc83
Takefumi MIYOSHI <miyo@wasamon.net>
2018-09-18 18:58:13 +0900

    add large_case

---
+++ large_case/gen_large_case.rb
@@ -0,0 +1,29 @@
+NUM=2**13
+MAX=2**31-1
+
+def gen_switch(i)
+  return "case #{i}: ret = v ^ #{rand(MAX)}; break;\n"
+end
+
+puts <<EOF
+
+class B{
+      public int t(int k, int v){
+        int ret = 0;
+        switch(k){
+EOF
+NUM.times{ |i| puts gen_switch(i) }
+puts <<EOF
+          default: ret = 0;
+        }
+        return ret;
+      }
+}
+
+public class A{
+       public static void main(String... args){
+              B b = new B();
+              System.out.println(b.t(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+       }
+}
+EOF
